19b80e6ebfb057a8ada76e8e36acfb76fd9e1990
bijja <balu2java.bijja@gmail.com>
2017-09-15 12:58:33 +0530

    added

--- Duplicates.java
+++ Duplicates.java
@@ -29,10 +29,10 @@ public class Duplicates {
     }
      
     public static void main(String a[]){
-        int[] input1 = {2,3,6,6,8,9,10,10,10,12,12};
+        int[] input1 = {2,3,6,6,8,9,10,10,10,12,12,13,14,14,15,15,15,16};
         int[] output = removeDuplicates(input1);
         for(int i:output){
-            System.out.print(i+" i am learnning git branch creation");
+            System.out.print(i+" i am learnning git merging to another branch");
 			 System.out.print(i+" "+"I am bala added new YSSO");
         }
     }
