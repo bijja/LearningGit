d16e40bdc70131d09b59bd636fa2fd94ac86068c
bijja <balu2java.bijja@gmail.com>
2017-09-15 13:08:53 +0530

    added1313

--- Duplicates.java
+++ Duplicates.java
@@ -29,7 +29,7 @@ public class Duplicates {
     }
      
     public static void main(String a[]){
-        int[] input1 = {2,3,6,6,8,9,10,10,10,12,12};
+        int[] input1 = {2,3,6,6,8,9,10,10,10,12,12,13,13};
         int[] output = removeDuplicates(input1);
         for(int i:output){
             System.out.print(i+" i am learnning git branch creation");
